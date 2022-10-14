d9023e7869784e5ede7955f8bbe0e5a556bd8790
immel-f <53239370+immel-f@users.noreply.github.com>
2022-10-14 18:02:33 +0200

    Update index.js

--- dist/main/index.js
+++ dist/main/index.js
@@ -18524,7 +18524,7 @@ async function run() {
 			    commit_sha: options.commit,
 			    body: body,
 		    })
-      
+      	}
       } else {
 	      
       core.info("github-token received is empty. Skipping writing a comment.");
